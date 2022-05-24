224eeb415064d9a522dec7113e0a77140c3069b7
leeseungseok <new18@ggook.com>
2022-05-24 17:44:43 +0900

    테스트코드 완료

--- ObjectStorage/bulkdata.php
+++ ObjectStorage/bulkdata.php
@@ -26,7 +26,9 @@ $objectKey = '3m92228957q_personalRecommend_c92h9lsw5z6';
 // 3m92228957q_personalRecommend_c92h9lsw5z6 & q3k2uukkypo_pop_q6elqefuloe
 try 
 {
-    // Object Lists 를 반환
+    /*-----------------------------------------------------------------
+    Object Lists (FileNames) 를 반환
+    -----------------------------------------------------------------*/
     $objects = $s3Client->listObjects([
         'Bucket' => $bucket
     ]);
@@ -36,46 +38,86 @@ try
         if(strpos($object['Key'], $objectKey))
         {
             $str    = explode('/', $object['Key']);
-            $keys[] = $str[2];
+            $fileNames[] = $str[2];
         }
     }
 
-    // Object List 의 데이터를 읽어옴
+    /*-----------------------------------------------------------------
+    Object List 의 데이터를 읽어와 date 추가 및 파싱
+    -----------------------------------------------------------------*/
     $bucket = 'aitems-4232530859020/infer_result/' . $objectKey;
 
-    foreach($keys as $key)
+    foreach($fileNames as $fileName)
     {
         $result = $s3Client->getObject([
             'Bucket' => $bucket,
-            'Key'    => $key,
+            'Key'    => $fileName,
         ]);
 
         // GuzzleHttp\Psr7\Stream
         $data    = $result['Body'];
         $data    = $data->getContents();
-        
-        // json_decode 가능한 형태로 수정
-        $data  = str_replace("}\n", "},", $data);
-        $data  = substr($data, 0, -1);
-        $data  = '[' . $data . ']';
 
-        $data  = json_decode($data, true);
+        $now = date("Y-m-d H:i:s");
 
-        $i = 1;
-        foreach($data as $v)
+        $i = 0;
+        $data = explode("\n", $data);
+        foreach($data as $array)
         {
-            $date    = array('date' => date('Y-m-d H:i:s'));
-            $data    = array_merge($v, $date);
-            $datas[] = $data;
-            if($i == 5)
+            // $array = 하나의 json 파일 안의 데이터 1개
+            $array    = json_decode($array, true);
+            
+            // $eduData = 데이터 1개 + date
+            $date     = array('date' => $now);
+            $eduData  = array_merge($array, $date);
+            $eduData  = json_encode($eduData);
+
+            // [Set] ElasticSearch Header
+            $header   = array();
+            $header[] = 'Content-Type: application/json';
+            $header[] = 'Authorization: Basic Y3VyYXRpb246YWl0ZW1zMjAyMiEh';
+            
+            // [Set] index + eduData
+            $body = array("name"=>"curation_personal", "value"=>"side_b", "date"=>$now, "data"=>$eduData);
+            $body = json_encode($body);
+
+            // curation_personal_side_a/user_id
+            $url = 'http://211.34.235.243:9200/curation_personal_side_b/_doc/' . $array['USER_ID'];
+            $ch  = curl_init(); 
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30); // 목적지 서버상태가 Overload 상태인지, 또는 크리티컬한 에러가 있는 상태인지에 따라 적용
+            curl_setopt($ch, CURLOPT_TIMEOUT, 30);        // 굉장히 큰파일, 또는 느린 연결 속도(네트워크속도에 좌우됨) 등에 따라 적용
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+
+            $res_raw = curl_exec($ch);
+            $error   = curl_errno($ch);
+
+            if($error)
             {
-                break;
+                throw new Exception('엘라스틱 서치 PUT CURL 통신에 실패하였습니다. / ' .$error);
             }
+
+            curl_close($ch);
+
+            $res_decode = json_decode($res_raw, true);
+            var_dump($res_decode);
+            // exit;
+            
+            usleep(500);
             $i++;
+            if($i == 100)
+            {
+                exit;
+            }
         }
+        // print_r($eduData);
     }
 
-    // $datas = json_encode($datas);
+    
     $datas = json_encode($datas, JSON_PRETTY_PRINT);
     var_dump($datas);
     exit;
